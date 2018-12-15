37e21d7e462de0bfb6fc76af339e93c72c34a292
Mark Lee <electronjs@lazymalevolence.com>
2018-12-14 21:55:58 -0800

    Remove unnecessary test deduplication

--- test/gethomepage.js
+++ test/gethomepage.js
@@ -3,52 +3,41 @@
 const getHomePage = require('../src/gethomepage')
 const test = require('ava')
 
-const scenarios = [
-  {
-    name: 'Return empty string if none available',
-    pkg: {},
-    expectedURL: ''
-  },
-  {
-    name: 'Use homepage property if present',
-    pkg: {
-      homepage: 'http://example.com/homepage-property',
-      author: 'First Last <first.last@example.com> (http://www.example.com/author-string)'
-    },
-    expectedURL: 'http://example.com/homepage-property'
-  },
-  {
-    name: 'Use URL from author string if no homepage',
-    pkg: {
-      author: 'First Last <first.last@example.com> (http://www.example.com/author-string)'
-    },
-    expectedURL: 'http://www.example.com/author-string'
-  },
-  {
-    name: 'Use URL from author object if no homepage',
-    pkg: {
-      author: {
-        url: 'http://www.example.com/author-object-url'
-      }
-    },
-    expectedURL: 'http://www.example.com/author-object-url'
-  },
-  {
-    name: 'undefined if neither author string has no URL nor homepage is specified',
-    pkg: {
-      author: 'Alice B.'
-    },
-    expectedURL: undefined
-  },
-  {
-    name: 'blank if author name is present but neither homepage nor author URL is specified',
-    pkg: {
-      author: { name: 'Alice B.' }
-    },
-    expectedURL: ''
-  }
-]
+test(
+  'Return empty string if none available',
+  t => t.is(getHomePage({}), '')
+)
 
-for (const scenario of scenarios) {
-  test(scenario.name, t => t.is(getHomePage(scenario.pkg), scenario.expectedURL))
-}
+test(
+  'Use homepage property if present',
+  t => t.is(getHomePage({
+    homepage: 'http://example.com/homepage-property',
+    author: 'First Last <first.last@example.com> (http://www.example.com/author-string)'
+  }), 'http://example.com/homepage-property')
+)
+
+test(
+  'Use URL from author string if no homepage',
+  t => t.is(
+    getHomePage({ author: 'First Last <first.last@example.com> (http://www.example.com/author-string)' }),
+    'http://www.example.com/author-string'
+  )
+)
+
+test(
+  'Use URL from author object if no homepage',
+  t => t.is(
+    getHomePage({ author: { url: 'http://www.example.com/author-object-url' } }),
+    'http://www.example.com/author-object-url'
+  )
+)
+
+test(
+  'undefined if neither author string has no URL nor homepage is specified',
+  t => t.is(getHomePage({ author: 'Alice B.' }), undefined)
+)
+
+test(
+  'blank if author name is present but neither homepage nor author URL is specified',
+  t => t.is(getHomePage({ author: { name: 'Alice B.' } }), '')
+)
