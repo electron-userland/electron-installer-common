8a9d31b87be96c9acb4d572ef615b2d8c5e9c4d4
Mark Lee <malept@users.noreply.github.com>
2018-12-26 11:58:54 -0800

    Don't require an options object to use readElectronVersion (#3)
    Breaking change to API.

--- test/readelectronversion.js
+++ test/readelectronversion.js
@@ -1,9 +1,10 @@
 'use strict'
 
+const path = require('path')
 const readElectronVersion = require('../src/readelectronversion')
 const test = require('ava')
 
 test('readElectronVersion', t => {
-  return readElectronVersion({ src: 'test/fixtures' })
+  return readElectronVersion(path.resolve(__dirname, 'fixtures'))
     .then(version => t.is(version, 'v3.0.11'))
 })
