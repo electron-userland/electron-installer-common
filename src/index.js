d5fcc562aef92b8c36a4fb7a046ecec281fbc182
Mark Lee <electronjs@lazymalevolence.com>
2019-01-14 23:52:36 -0800

    Re-export getHomePage

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const desktop = require('./desktop')
 const ElectronInstaller = require('./installer')
 const error = require('./error')
 const getDefaultsFromPackageJSON = require('./defaults')
+const getHomePage = require('./gethomepage')
 const readElectronVersion = require('./readelectronversion')
 const readMetadata = require('./readmetadata')
 const replaceScopeName = require('./replacescopename')
@@ -22,6 +23,7 @@ module.exports = {
   getDepends: dependencies.getDepends,
   getGConfDepends: dependencies.getGConfDepends,
   getGTKDepends: dependencies.getGTKDepends,
+  getHomePage,
   getTrashDepends: dependencies.getTrashDepends,
   getUUIDDepends: dependencies.getUUIDDepends,
   mergeUserSpecified: dependencies.mergeUserSpecified,
