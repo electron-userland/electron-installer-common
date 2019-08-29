5c2a38a9f8dfc211e982511da805209b973dfbe8
Mark Lee <electronjs@lazymalevolence.com>
2019-08-28 22:55:33 -0700

    chore: fix lint warnings

--- test/gethomepage.js
+++ test/gethomepage.js
@@ -5,39 +5,45 @@ const test = require('ava')
 
 test(
   'Return empty string if none available',
-  t => t.is(getHomePage({}), '')
+  t => { t.is(getHomePage({}), '') }
 )
 
 test(
   'Use homepage property if present',
-  t => t.is(getHomePage({
-    homepage: 'http://example.com/homepage-property',
-    author: 'First Last <first.last@example.com> (http://www.example.com/author-string)'
-  }), 'http://example.com/homepage-property')
+  t => {
+    t.is(getHomePage({
+      homepage: 'http://example.com/homepage-property',
+      author: 'First Last <first.last@example.com> (http://www.example.com/author-string)'
+    }), 'http://example.com/homepage-property')
+  }
 )
 
 test(
   'Use URL from author string if no homepage',
-  t => t.is(
-    getHomePage({ author: 'First Last <first.last@example.com> (http://www.example.com/author-string)' }),
-    'http://www.example.com/author-string'
-  )
+  t => {
+    t.is(
+      getHomePage({ author: 'First Last <first.last@example.com> (http://www.example.com/author-string)' }),
+      'http://www.example.com/author-string'
+    )
+  }
 )
 
 test(
   'Use URL from author object if no homepage',
-  t => t.is(
-    getHomePage({ author: { url: 'http://www.example.com/author-object-url' } }),
-    'http://www.example.com/author-object-url'
-  )
+  t => {
+    t.is(
+      getHomePage({ author: { url: 'http://www.example.com/author-object-url' } }),
+      'http://www.example.com/author-object-url'
+    )
+  }
 )
 
 test(
   'undefined if neither author string has no URL nor homepage is specified',
-  t => t.is(getHomePage({ author: 'Alice B.' }), undefined)
+  t => { t.is(getHomePage({ author: 'Alice B.' }), undefined) }
 )
 
 test(
   'blank if author name is present but neither homepage nor author URL is specified',
-  t => t.is(getHomePage({ author: { name: 'Alice B.' } }), '')
+  t => { t.is(getHomePage({ author: { name: 'Alice B.' } }), '') }
 )
