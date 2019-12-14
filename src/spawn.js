77a4f3cc3e0262c529e01344ec07f01719a734b9
Mark Lee <electronjs@lazymalevolence.com>
2019-12-14 11:43:30 -0800

    refactor: replace cross-spawn-promise with cross-spawn

--- src/spawn.js
+++ src/spawn.js
@@ -1,24 +1,39 @@
 'use strict'
 
-const spawn = require('cross-spawn-promise')
+const spawn = require('cross-spawn')
 
 /**
  * Spawn a child process and make the error message more human friendly, if possible.
  *
+ * If logger is specified, it's usually a debug or console.log function pointer.
  * Specify updateErrorCallback (a callback) to adjust the error object before it is rethrown.
  */
 module.exports = async function (cmd, args, logger, updateErrorCallback) {
   if (logger) logger(`Executing command ${cmd} ${args.join(' ')}`)
 
-  try {
-    const stdout = await spawn(cmd, args)
-    return stdout.toString()
-  } catch (err) {
-    const stderr = err.stderr ? err.stderr.toString() : ''
-    if (updateErrorCallback) {
-      updateErrorCallback(err, !!logger)
-    }
-
-    throw new Error(`Error executing command (${err.message || err}):\n${cmd} ${args.join(' ')}\n${stderr}`)
-  }
+  return new Promise((resolve, reject) => {
+    let stdout = ''
+    let stderr = ''
+    const process = spawn(cmd, args)
+    process.stdout.on('data', data => {
+      stdout += data.toString()
+    })
+    process.stderr.on('data', data => {
+      /* istanbul ignore next */
+      stderr += data.toString()
+    })
+    process.on('close', code => {
+      if (code === 0) {
+        resolve(stdout)
+      } else {
+        reject(new Error(`Command failed with a non-zero return code (${code}):\n${cmd} ${args.join(' ')}\n${stdout}\n${stderr}`))
+      }
+    })
+    process.on('error', err => {
+      if (updateErrorCallback) {
+        updateErrorCallback(err, !!logger)
+      }
+      reject(new Error(`Error executing command (${err.message || err}):\n${cmd} ${args.join(' ')}\n${stderr}`))
+    })
+  })
 }
