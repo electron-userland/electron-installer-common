6a0a6f0f2a4bb183c24f6d41a2a8fe5a8550cbff
Mark Lee <electronjs@lazymalevolence.com>
2020-01-21 10:15:06 -0800

    refactor: upgrade ava to ^3 and eslint-plugin-ava to ^10

--- test/error.js
+++ test/error.js
@@ -14,11 +14,11 @@ test('errorMessage with Error sans message', t => {
 test('wrapError', t => {
   const promise = Promise.reject(new Error('My error'))
     .catch(error.wrapError('in a test'))
-  return t.throwsAsync(promise, /Error in a test: My error/)
+  return t.throwsAsync(promise, { message: /Error in a test: My error/ })
 })
 
 test('wrapError with wrappedFunction specified', t => {
   t.throws(() => error.wrapError('in a test', () => {
     throw new Error('My error')
-  }), 'Error in a test: My error')
+  }), { message: 'Error in a test: My error' })
 })
