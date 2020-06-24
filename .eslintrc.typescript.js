377d92be999b6e57c7f1465296cb71aca13907ad
Mark Lee <electronjs@lazymalevolence.com>
2020-06-24 08:18:22 -0700

    build: Fix up eslint node rule for TypeScript

--- .eslintrc.typescript.js
+++ .eslintrc.typescript.js
@@ -5,7 +5,7 @@ eslintConfig.extends.push('plugin:@typescript-eslint/recommended')
 eslintConfig.rules = {
   'comma-dangle': ['error', 'only-multiline'],
   'no-useless-constructor': 'off',
-  'node/no-unsupported-features/es-syntax': 'off',
+  'node/no-unsupported-features/es-syntax': ['error', { ignores: ['modules'] }],
   semi: ['error', 'always'],
   'space-before-function-paren': ['error', 'never']
 }
